5e39aaab35198d7462af6523510755fecf428967
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-12-24 00:14:52 -0800

    update

---
+++ s3-test.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+#test = pd.read_csv('s3://tridata/Santa-Cruz-Sprint.csv')
+#females = test[test['Gender']=='F']
+
+data = [[20, 10], [30, 15], [25, 14]]
+females = pd.DataFrame(data, columns = ['Age', 'Gender Place'])
+
+#print(test)
+#print(females)
+
+fig = px.scatter(females, x=females['Age'], y=females['Gender Place'])
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
