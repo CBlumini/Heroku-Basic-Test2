e1a2cf525d8bfd89872b32dd2cbe3dda11fbaac2
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-12-27 23:08:56 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,141 @@ from dash import dcc
 from dash import html
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
+import dash_table
 
 app = dash.Dash(__name__)
-
 server = app.server
 
-test = pd.read_csv('https://github.com/CBlumini/heroku_dep_2/raw/main/Santa-Cruz-Sprint.csv', header=0, index_col=None)
+#set some colors
+colors = {
+    'background': '#111111',
+    'text': '#00ABE1'
+}
 
-#test = pd.read_csv('s3://tridata/Santa-Cruz-Sprint.csv')
+#ingest data
+test = pd.read_csv('https://github.com/CBlumini/heroku_dep_2/raw/main/Santa-Cruz-Sprint.csv', header=0, index_col=None)
+data = test
 females = test[test['Gender']=='F']
 
-#data = [[20, 10], [30, 15], [25, 14]]
-#females = pd.DataFrame(data, columns = ['Age', 'Gender Place'])
 
-#print(test)
-#print(females)
+#the data does not come in the right form to do math on it. So convert the times to minutes and decimal seconds
+#maybe setup a compute file to do this by itself later
+def create_time_columns(bare_frame):
+    def convertTime (time):
+        temp = time.split(':')
+        timeMinutes = (int(temp[0])*60)+int(temp[1])+int(temp[2])/60
+        return timeMinutes
 
+    #convert to integers
+    bare_frame["Swim Minutes"] = bare_frame["Swim"].apply(convertTime)
+    bare_frame["T1 Minutes"] = bare_frame["T1"].apply(convertTime)
+    bare_frame["Bike Minutes"] = bare_frame["Bike"].apply(convertTime)
+    bare_frame["T2 Minutes"] = bare_frame["T2"].apply(convertTime)
+    bare_frame["Run Minutes"] = bare_frame["Run"].apply(convertTime)
+    #bare_frame["Elapsed Minutes"] = bare_frame["Chip Elapsed"].apply(convertTime)
+
+    #create cumulative times
+    bare_frame["Swim+T1"]=round(bare_frame["Swim Minutes"]+bare_frame["T1 Minutes"], 2)
+    bare_frame["Plus Bike"]=round(bare_frame["Swim+T1"]+bare_frame["Bike Minutes"], 2)
+    bare_frame["Plus T2"]=round(bare_frame["Plus Bike"]+bare_frame["T2 Minutes"], 2)
+    bare_frame["Total"]=round(bare_frame["Plus T2"]+bare_frame["Run Minutes"], 2)
+
+    return bare_frame
+
+
+time_df = create_time_columns(data)
+
+reduced2 = time_df[["Name","Swim Minutes","Swim+T1","Plus Bike","Plus T2","Total","Gender Place"]]
+reduced2["Start"] = 0
+
+#create the para coord plot
+dimensions = list([
+            dict(range = [0, 1],
+                label = 'Start', values = reduced2['Start']),            
+            dict(range = [reduced2["Swim Minutes"].min(), reduced2["Swim Minutes"].max()],
+                label = 'Time After Swim', values = reduced2['Swim Minutes']),
+            dict(range = [reduced2["Swim+T1"].min(), reduced2["Swim+T1"].max()],
+                label = 'Time After First Transition', values = reduced2['Swim+T1']),
+            dict(range = [reduced2["Plus Bike"].min(), reduced2["Plus Bike"].max()],
+                label = 'Time After Bike', values = reduced2['Plus Bike']),
+            dict(range = [reduced2["Plus T2"].min(), reduced2["Plus T2"].max()],
+                label = 'Time After Second Transition', values = reduced2['Plus T2']),
+            dict(range = [reduced2["Total"].min(), reduced2["Total"].max()],
+                label = 'Total Time', values = reduced2['Total']),
+            dict(range=[0,reduced2['Gender Place'].max()], tickvals = reduced2['Gender Place'], ticktext = reduced2['Name'],
+                    label='Competitor', values=reduced2['Gender Place'])
+        ])
+
+para_cor = go.Figure(data=go.Parcoords(line = dict(color = reduced2['Gender Place'],
+                colorscale = [[.0,'rgba(255,0,0,0.1)'],[0.2,'rgba(0,255,0,0.1)'],[.4,'rgba(0,0,255,0.1)'], 
+                                [.6,'rgba(0,255,255,0.1)'], [.8, 'rgba(255,0,255,0.1)'], [1, 'rgba(0,0,0,0.1)']]), dimensions=dimensions))
+
+para_cor.update_layout(
+    title="Triathalon Results",
+    width=1920,
+    height=1080)
+
+
+#create the plots
 fig = px.scatter(females, x=females['Age'], y=females['Gender Place'])
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+#create a data table, name the columns
+dash_columns=["Bib", "Name", "Age", "Gender", "Team Name", "City", "Swim", "T1", "Bike", "T2", "Run", "Chip Elapsed", "Div Place", "Age Place", 
+              "Gender Place"]
 
-app.layout = html.Div(children=[
-    html.H1(children='Hi Samantha!!!'),
+#layout the page
+app.layout = html.Div(style={'backgroundColor': colors['background']},  
+children=[
+    html.H1(
+        children='Welcome to Triathalon Data Analyzer',
+        style={
+            'textAlign': 'center',
+            'color': colors['text'] 
+            }),
 
-    html.Div(children='''
-        This is some data from the Women at the Sata Cruz Sprint!
-    '''),
+    html.Div(children='This app allows for performance plotting of certain local bay area triathalons.',
+    style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    
+    html.Div(dash_table.DataTable(
+                            id='table-sorting-filtering',
+                            columns=[{'name': i, 'id': i} for i in dash_columns],
+                            data=time_df.to_dict('records'),
+                            style_table={'overflowX': 'scroll',},
+                                style_header={
+                                             'backgroundColor': 'rgb(30, 30, 30)',
+                                              'color': 'white'
+                                            },
+                            style_data={
+                                             'backgroundColor': 'rgb(50, 50, 50)',
+                                           'color': 'white'
+                                            },
+                            style_cell={
+                                'height': '90',
+                                #'minWidth': '110%',
+                                'minWidth': '60px', 'width': '100px', 'maxWidth': '140px',
+                                'whiteSpace': 'normal', 'textAlign': 'center'},
+                            style_cell_conditional=[        {
+                                'if': {'column_id': 'Name'},
+                                'textAlign': 'center'
+                                    }],
+                            page_current= 0,
+                            page_size=15,
+                            filter_action='native',
+                            filter_query='',
+                            sort_action='native',
+                            sort_mode='single',
+                            sort_by=[],
+                            style_as_list_view=True,
+                            hidden_columns=[],
+                        )),
 
     dcc.Graph(
         id='example-graph',
